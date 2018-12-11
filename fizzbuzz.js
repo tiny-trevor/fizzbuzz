f9e8462bfb2869d71f356e91dd3e3239fe27e313
Naomi Trevor <naomi.e.trevor@gmail.com>
2018-12-11 12:12:41 +0000

    Update fizzbuzz.js
    Added a way to determine how far the function will count

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,6 +1,8 @@
-for (i = 1; i <= 100; i++) {
-  if (i % 3 == 0 && i % 4 != 0) document.write("fizz<br />");
-  else if (i % 4 == 0 && i % 3 != 0) document.write("buzz<br />");
-  else if (i % 4 == 0 && i % 3 == 0) document.write("fizzbuzz<br />");
-  else document.write(i+"<br />");
+function fizzbuzz(x)
+  for (i = 1; i <= x; i++) {
+    if (i % 3 == 0 && i % 4 != 0) document.write("fizz<br />");
+    else if (i % 4 == 0 && i % 3 != 0) document.write("buzz<br />");
+    else if (i % 4 == 0 && i % 3 == 0) document.write("fizzbuzz<br />");
+    else document.write(i+"<br />");
+  }
 }
